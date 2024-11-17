09ca478c452236dc6eb59c4cc395820ea5cbfa2f
YevhenKataiev <kataev.evgen.1989@gmail.com>
2024-11-17 16:32:41 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-
+      <div> Logo</div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
